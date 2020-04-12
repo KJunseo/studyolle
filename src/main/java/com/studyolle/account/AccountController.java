8338ad9f72ebd316db2f7123b968403a7796e09c
KJunseo <max9106@naver.com>
2020-04-12 22:16:45 +0900

    회원가입 처리(db에 저장)

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,5 +1,6 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
 
     /* signUpForm이라는 데이터를 받을 때 바인더 설정
        (더해준 validator도 자동으로 검사해준다.) */
@@ -37,6 +39,17 @@ public class AccountController {
         if(errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO 후에 password encoding 필요
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
 
         return "redirect:/";
     }
