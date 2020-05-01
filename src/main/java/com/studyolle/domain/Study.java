227bf8b54e6733d58e99ae8733a5e28e505117c5
KJunseo <max9106@naver.com>
2020-05-02 01:04:41 +0900

    스터디 도메인 엔티티 생성

---
+++ src/main/java/com/studyolle/domain/Study.java
@@ -0,0 +1,55 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Study {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers; // 후에 권한을 위임할 경우도 있기 때문에 여러명으로 설정
+
+    @ManyToMany
+    private Set<Account> members;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER) // 즉시 로딩(무조건 가져오기)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags;
+
+    @ManyToMany
+    private Set<Zone> zones;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+}
