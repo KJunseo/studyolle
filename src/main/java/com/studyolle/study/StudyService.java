5d2e78fc10b945e5456e98ab79ad68bc723145c1
KJunseo <max9106@naver.com>
2020-05-05 00:20:45 +0900

    스터디 description  수정

--- src/main/java/com/studyolle/study/StudyService.java
+++ src/main/java/com/studyolle/study/StudyService.java
@@ -2,9 +2,13 @@ package com.studyolle.study;
 
 import com.studyolle.domain.Account;
 import com.studyolle.domain.Study;
+import com.studyolle.study.form.StudyDescriptionForm;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.security.access.AccessDeniedException;
 
 @Service
 @Transactional
@@ -12,10 +16,34 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudyService {
 
     private final StudyRepository studyRepository;
+    private final ModelMapper modelMapper;
 
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = studyRepository.save(study);
         newStudy.addManager(account);
         return newStudy;
+    }
+
+    public Study getStudyToUpdate(Account account, String path) {
+        Study study = this.getStudy(path);
+
+        // 매니저가 아니면 사용 불가
+        if(!account.isManagerOf(study)) {
+            throw new AccessDeniedException("해당 기능을 사용할 수 없습니다.");
+        }
+        return study;
+    }
+
+    public Study getStudy(String path) {
+        Study study = this.studyRepository.findByPath(path);
+
+        if(study==null) {
+            throw new IllegalArgumentException(path + "에 해당하는 스터디가 없습니다.");
+        }
+        return study;
+    }
+
+    public void updateStudyDescription(Study study, StudyDescriptionForm studyDescriptionForm) {
+        modelMapper.map(studyDescriptionForm, study);
     }
 }
